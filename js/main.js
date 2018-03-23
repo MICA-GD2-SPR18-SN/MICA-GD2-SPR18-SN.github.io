82af28fa8efacac2500a5f223b6af2f05f523a4c
Spence Nelson <spenjmn@gmail.com>
2018-03-22 22:40:56 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@
 $( document ).ready(function() {
     $( "#nav" ).load( "../nav.html" );
 });
+
+$( document ).ready(function() {
+  $(".main-nav .openlink").click(function(){
+    $(".main-nav .navitems").toggleClass("open");
+  });
+
+  $(".main-nav .closelink").click(function(){
+    $(".main-nav .navitems").toggleClass("open");
+  });
+});
